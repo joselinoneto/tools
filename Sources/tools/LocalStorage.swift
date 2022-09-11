790966a1a0e753a35d7bed5a5527a5e5cb65d169
José Neto <jneto@blissapplications.com>
2022-09-11 22:23:42 +0100

    storage tools

---
+++ Sources/tools/LocalStorage.swift
@@ -0,0 +1,40 @@
+//
+//  LocalStorage.swift
+//  
+//
+//  Created by José Neto on 11/09/2022.
+//
+
+import Foundation
+
+public class FileStorage {
+    let fileExtension: String = ".jpg"
+    public static let shared: FileStorage = FileStorage()
+    
+    private var folderUrl: URL? {
+        //FileManager.default.containerURL(forSecurityApplicationGroupIdentifier: "group.com.zeneto.astronomia-app")
+        try? FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: false)
+    }
+    
+    public func getLocalFile(fileName: String) -> URL? {
+        let filePath = folderUrl?.appendingPathComponent("\(fileName)\(fileExtension)")
+        guard let filePathString: String = filePath?.path else { return nil }
+        return FileManager.default.fileExists(atPath: filePathString) ? filePath : nil
+    }
+    
+    public func saveRemoteFile(imageUrl: URL?, fileName: String?) async throws {
+        guard let folderUrl = folderUrl else { return }
+        guard let imageUrl = imageUrl else { return }
+        guard let fileName = fileName else { return }
+        let fileURL = folderUrl.appendingPathComponent("\(fileName)\(fileExtension)")
+        if FileManager.default.fileExists(atPath: fileURL.path) {
+            return 
+        }
+        do {
+            let data = try await URLSession.shared.download(from: imageUrl)
+            try FileManager.default.moveItem(atPath: data.0.path, toPath: fileURL.path)
+        } catch {
+            // TODO: Some log
+        }
+    }
+}
