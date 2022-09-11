06059de2e5f348dca56bd2d5af23b818c10fd9b4
José Neto <jneto@blissapplications.com>
2022-09-11 22:43:31 +0100

    try find local asset in storage

--- Sources/tools/LocalStorage.swift
+++ Sources/tools/LocalStorage.swift
@@ -16,10 +16,15 @@ public class FileStorage {
         try? FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: false)
     }
     
+    private var assetsPath: URL? {
+        FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask)[0]
+    }
+    
     public func getLocalFile(fileName: String) -> URL? {
         let filePath = folderUrl?.appendingPathComponent("\(fileName)\(fileExtension)")
+        let assetPath = assetsPath?.appendingPathComponent("\(fileName)\(fileExtension)")
         guard let filePathString: String = filePath?.path else { return nil }
-        return FileManager.default.fileExists(atPath: filePathString) ? filePath : nil
+        return FileManager.default.fileExists(atPath: filePathString) ? filePath : assetPath
     }
     
     public func saveRemoteFile(imageUrl: URL?, fileName: String?) async throws {
